015c8a5c67376ddadf98bd68fab267b598ce571b
RiptideWallace <brysonc1987@gmail.com>
2017-06-29 21:16:16 +0000

    first run at custom map, will come back to try different things with it

---
+++ mapcustom.js
@@ -0,0 +1,8 @@
+var words = ["spaghetti", "penne", "tortellini", "bowtie", "noodle"];
+
+var list = words.map(wordlist);
+function wordlist(word) {
+    return word.length;
+}
+
+  console.log(list)
