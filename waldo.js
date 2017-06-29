3662084124a192260b3460c9609f288ddb9f0036
RiptideWallace <brysonc1987@gmail.com>
2017-06-29 18:30:31 +0000

    waldo exercise finding where in the array he is, before refactoring

--- waldo.js
+++ waldo.js
@@ -2,14 +2,13 @@
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
-      found();   // execute callback
+      found(i);   // execute callback
     }
   }
 }
 
-function actionWhenFound() {
-  var people = ["Alice", "Bob", "Waldo", "Winston"];
-  console.log("Found Waldo at index " + people.indexOf("Waldo") + "!");
+function actionWhenFound(indexOf) {
+  console.log("Found Waldo at index " + indexOf + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
